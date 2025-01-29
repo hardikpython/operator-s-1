06426b465046abac8ea2b840f272595c0a28758f
hardikpython <pk646155@gmail.com>
2025-01-29 22:22:21 +0530

    percentageofsubjects

---
+++ percentage.py
@@ -0,0 +1,8 @@
+print("enter marks obtained in 3subjects")
+math=int(input("enter math marks"))
+science=int(input("enter science marks"))
+socialscience=int(input("enter social sciece marks"))
+sum=math+science+socialscience
+print(sum)
+percentage= (sum/300)*100
+print(percentage)
