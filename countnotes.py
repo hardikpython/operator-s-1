6d9272eef3cfc4afb93dcdb1d280639d68ed9f17
hardikpython <pk646155@gmail.com>
2025-01-29 22:05:58 +0530

    countnotes.py

---
+++ countnotes.py
@@ -0,0 +1,8 @@
+#taking total amount as input from user
+amount=int(input("enter your amount"))
+note1=amount//100
+note2=(amount%100)//50
+note3=((amount%100)%50)//10
+print(note1)
+print(note2)
+print(note3)
