b00f40b86c3ab65ecf5007441f1065f724a6f695
hardikpython <pk646155@gmail.com>
2025-02-07 19:02:27 +0530

    progameusingstring

---
+++ conditionalstatementhw.py
@@ -0,0 +1,8 @@
+# Program to check if a number is positive, negative, or zero
+number = 5
+if number > 0:
+    print("The number is positive.")
+elif number < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
